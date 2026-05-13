6328dbdff04e0d2ee6007af37aa662fd7f3cd5f2
AlessioLopa <alessio.lopardo@protonmail.com>
2026-05-13 08:53:22 +0200

    test: add test name comment

--- backend/tests/functional/auth/index.spec.ts
+++ backend/tests/functional/auth/index.spec.ts
@@ -1,10 +1,11 @@
 import { test } from '@japa/runner'
 
 test.group('Register', () => {
+  // TF-USR-03
   test('Register without data', async ({ client }) => {
     const response = await client.post('/api/register').json({
       email: '',
-      password: ''
+      password: '',
     })
     response.assertStatus(422)
     response.assertBodyContains({
@@ -12,169 +13,175 @@ test.group('Register', () => {
         {
           message: 'The email field must be defined',
           field: 'email',
-          rule: 'required'
+          rule: 'required',
         },
         {
           message: 'The password field must be defined',
           field: 'password',
-          rule: 'required'
-        }
-      ]
+          rule: 'required',
+        },
+      ],
     })
   })
 
+  // TF-USR-04
   test('Register email not valid', async ({ client }) => {
     const response = await client.post('/api/register').json({
       email: 'test.com',
-      password: 'password'
+      password: 'password',
     })
     response.assertStatus(422)
     response.assertBodyContains({
       errors: [
         {
           message: 'The email field must be a valid email address',
-          rule: 'email'
-        }
-      ]
+          rule: 'email',
+        },
+      ],
     })
   })
 
+  // TF-USR-02
   test('Register with existing email', async ({ client }) => {
     const response = await client.post('/api/register').json({
       email: 'alessio.lopardo@etml.ch',
-      password: 'password'
+      password: 'password',
     })
     response.assertStatus(422)
     response.assertBodyContains({
-      errors: [{
-        message: "The email has already been taken",
-        rule: "database.unique",
-        field: "email"
-      }]
+      errors: [
+        {
+          message: 'The email has already been taken',
+          rule: 'database.unique',
+          field: 'email',
+        },
+      ],
     })
-
   })
 
+  // TF-USR-01
   test('Register valid user', async ({ client }) => {
     const response = await client.post('/api/register').json({
       email: 'user@test.ch',
-      password: 'password'
+      password: 'password',
     })
     response.assertStatus(201)
     response.assertBodyContains({
-      email: 'user@test.ch'
+      email: 'user@test.ch',
     })
-
-
   })
 })
 
-
 test.group('Login', () => {
+  // TF-USR-05
   test('Login with valid data', async ({ client }) => {
     const response = await client.post('/api/login').json({
       email: 'alessio.lopardo@etml.ch',
-      password: 'password'
+      password: 'password',
     })
     response.assertStatus(200)
-
   })
 
+  // TF-USR-06
   test('Login with incorrect password', async ({ client }) => {
     const response = await client.post('/api/login').json({
       email: 'alessio.lopardo@etml.ch',
-      password: 'wrongpassword'
-    })
-    response.assertStatus(400)
-    response.assertBodyContains({
-      errors: [{
-        message: 'Invalid user credentials'
-      }]
-    })
-  })
-
-
-  test('Login without data', async ({ client }) => {
-    const response = await client.post('/api/login').json({
-      email: '',
-      password: ''
-    })
-    response.assertStatus(422)
-    response.assertBodyContains({
-      errors: [
-        {
-          message: 'The email field must be defined',
-          field: 'email',
-          rule: 'required'
-        },
-        {
-          message: 'The password field must be defined',
-          field: 'password',
-          rule: 'required'
-        }
-      ]
-    })
-  })
-
-
-  test('Login email not valid', async ({ client }) => {
-    const response = await client.post('/api/login').json({
-      email: 'test.com',
-      password: 'password'
-    })
-    response.assertStatus(422)
-    response.assertBodyContains({
-      errors: [
-        {
-          message: 'The email field must be a valid email address',
-          rule: 'email'
-        }
-      ]
-    })
-  })
-
-
-  test('Login with non existant email', async ({ client }) => {
-    const response = await client.post('/api/login').json({
-      email: 'test@test.com',
-      password: 'password'
+      password: 'wrongpassword',
     })
     response.assertStatus(400)
     response.assertBodyContains({
       errors: [
         {
           message: 'Invalid user credentials',
-        }
-      ]
+        },
+      ],
     })
   })
 
+  // TF-USR-07
+  test('Login without data', async ({ client }) => {
+    const response = await client.post('/api/login').json({
+      email: '',
+      password: '',
+    })
+    response.assertStatus(422)
+    response.assertBodyContains({
+      errors: [
+        {
+          message: 'The email field must be defined',
+          field: 'email',
+          rule: 'required',
+        },
+        {
+          message: 'The password field must be defined',
+          field: 'password',
+          rule: 'required',
+        },
+      ],
+    })
+  })
+
+  // TF-USR-08
+  test('Login email not valid', async ({ client }) => {
+    const response = await client.post('/api/login').json({
+      email: 'test.com',
+      password: 'password',
+    })
+    response.assertStatus(422)
+    response.assertBodyContains({
+      errors: [
+        {
+          message: 'The email field must be a valid email address',
+          rule: 'email',
+        },
+      ],
+    })
+  })
+
+  // TF-USR-09
+  test('Login with non existant email', async ({ client }) => {
+    const response = await client.post('/api/login').json({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    response.assertStatus(400)
+    response.assertBodyContains({
+      errors: [
+        {
+          message: 'Invalid user credentials',
+        },
+      ],
+    })
+  })
 })
 
 test.group('Logout', () => {
+  // TF-USR-10
   test('Logout with valid token', async ({ client }) => {
     const login = await client.post('/api/login').json({
-      email: "alessio.lopardo@etml.ch",
-      password: "password"
+      email: 'alessio.lopardo@etml.ch',
+      password: 'password',
     })
 
-    const response = await client.post("/api/logout").headers({
-      "Authorization": `Bearer ${login.body().token.token}`
+    const response = await client.post('/api/logout').headers({
+      Authorization: `Bearer ${login.body().token.token}`,
     })
     response.assertStatus(200)
     response.assertBodyContains({
-      message: "Logged out"
+      message: 'Logged out',
     })
   })
 
+  // TF-USR-11
   test('Logout without valid token', async ({ client }) => {
-
-    const response = await client.post("/api/logout")
+    const response = await client.post('/api/logout')
     response.assertStatus(401)
     response.assertBodyContains({
-      errors: [{
-        message: "Unauthorized access"
-      }]
+      errors: [
+        {
+          message: 'Unauthorized access',
+        },
+      ],
     })
   })
 })
