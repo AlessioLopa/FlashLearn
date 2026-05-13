cb6964be0ac368cb9285d960153e9d3f8d22a157
AlessioLopa <alessio.lopardo@protonmail.com>
2026-05-13 08:56:19 +0200

    refactor: prettier

--- backend/app/validators/auth.ts
+++ backend/app/validators/auth.ts
@@ -1,17 +1,23 @@
 import vine from '@vinejs/vine'
 
-export const loginValidator = vine.compile(vine.object({
+export const loginValidator = vine.compile(
+  vine.object({
     email: vine.string().email().minLength(3).maxLength(254),
-    password: vine.string().minLength(8).maxLength(255)
-}))
+    password: vine.string().minLength(8).maxLength(255),
+  })
+)
 
-
-export const registerValidator = vine.compile(vine.object({
-    email: vine.string().email().minLength(3).maxLength(254).unique(async (query, field) => {
-        const user = await query.from('users')
-            .where('email', field)
-            .first()
+export const registerValidator = vine.compile(
+  vine.object({
+    email: vine
+      .string()
+      .email()
+      .minLength(3)
+      .maxLength(254)
+      .unique(async (query, field) => {
+        const user = await query.from('users').where('email', field).first()
         return !user // Transform the value to Boolean for validation
-    }),
-    password: vine.string().minLength(8).maxLength(255)
-}))
+      }),
+    password: vine.string().minLength(8).maxLength(255),
+  })
+)
--- backend/app/validators/card.ts
+++ backend/app/validators/card.ts
@@ -1,8 +1,8 @@
 import vine from '@vinejs/vine'
 
 export const cardValidator = vine.compile(
-    vine.object({
-        recto: vine.string().minLength(1),
-        verso: vine.string().minLength(1),
-    })
+  vine.object({
+    recto: vine.string().minLength(1),
+    verso: vine.string().minLength(1),
+  })
 )
