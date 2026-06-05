ee7da63ba456ea6c5e3d8c953916fc931e65391d
AlessioLopa <alessio.lopardo@protonmail.com>
2026-06-05 13:52:13 +0200

    fix: add max lenght on cards validator

--- backend/app/validators/card.ts
+++ backend/app/validators/card.ts
@@ -2,7 +2,7 @@ import vine from '@vinejs/vine'
 
 export const cardValidator = vine.compile(
   vine.object({
-    recto: vine.string().minLength(1),
-    verso: vine.string().minLength(1),
+    recto: vine.string().minLength(1).maxLength(255),
+    verso: vine.string().minLength(1).maxLength(255),
   })
 )
